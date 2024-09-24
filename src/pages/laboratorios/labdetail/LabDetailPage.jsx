1bda7f8677a7f79cae3953bfb3f766bb8b4cfa2c
benjaminpartida <55998785+benjaminpartida@users.noreply.github.com>
2024-09-24 00:41:09 -0600

    Update LabDetailPage.jsx

--- src/pages/laboratorios/labdetail/LabDetailPage.jsx
+++ src/pages/laboratorios/labdetail/LabDetailPage.jsx
@@ -17,9 +17,8 @@ const AccordionItem = ({ title, content }) => {
       >
         <span className="text-lg font-medium">{title}</span>
         <svg
-          className={`w-6 h-6 transition-transform duration-300 transform ${
-            isOpen ? 'rotate-180' : 'rotate-0'
-          }`}
+          className={`w-6 h-6 transition-transform duration-300 transform ${isOpen ? 'rotate-180' : 'rotate-0'
+            }`}
           fill="none"
           stroke="currentColor"
           viewBox="0 0 24 24"
@@ -34,15 +33,15 @@ const AccordionItem = ({ title, content }) => {
         </svg>
       </button>
       <div
-        className={`overflow-hidden transition-all duration-300 ${
-          isOpen ? 'max-h-screen' : 'max-h-0'
-        }`}
+        className={`transition-all duration-300 ${isOpen ? 'max-h-full' : 'max-h-0'} overflow-hidden`}
       >
         <div className="p-4 text-gray-700">{content}</div>
       </div>
     </div>
   );
 };
+
+
 
 const Accordion = ({ items }) => (
   <div className="max-w-full p-2 mx-auto mt-0">
@@ -72,147 +71,167 @@ const LabDetailPage = () => {
     return <div>Loading...</div>; // Display loading message while data is being fetched
   }
 
-  // Prepare accordion items dynamically for services
   const accordionItems = lab.services?.length
-    ? lab.services.map((service) => ({
-        title: service.title,
-        content: (
-          <>
-            <p>{service.description}</p>
-            <table className="w-full border-collapse border border-gray-300 mb-4 mt-6">
-              <thead>
+  ? lab.services.map((service) => ({
+      title: service.title,
+      content: (
+        <>
+          <p className="mb-4 text-gray-600">{service.description}</p>
+          <div className="overflow-y-auto max-h-[60vh]">
+            {/* Ensure the table uses available width without horizontal scroll */}
+            <table className="table-fixed w-full text-left border-collapse bg-white rounded-lg shadow-sm">
+              <thead className="bg-gray-100 border-b border-gray-300">
                 <tr>
-                  <th className="border border-gray-300 p-2 bg-gray-100">Equipo</th>
-                  <th className="border border-gray-300 p-2 bg-gray-100">Muestra</th>
+                  <th className="p-4 text-gray-600 font-semibold">Equipo</th>
+                  <th className="p-4 text-gray-600 font-semibold">Muestra</th>
                 </tr>
               </thead>
               <tbody>
                 {service.equipment.map((eq, index) => (
-                  <tr key={index}>
-                    <td className="border border-gray-300 p-2">{eq.name}</td>
-                    <td className="border border-gray-300 p-2">{eq.range}</td>
+                  <tr
+                    key={index}
+                    className={`hover:bg-gray-100 transition-colors duration-300 ${
+                      index % 2 === 0 ? 'bg-gray-50' : 'bg-white'
+                    }`}
+                  >
+                    <td className="p-4 text-gray-700 break-words">{eq.name}</td>
+                    <td className="p-4 text-gray-700 break-words">{eq.range}</td>
                   </tr>
                 ))}
               </tbody>
             </table>
-          </>
-        ),
-      }))
-    : [];
+          </div>
+        </>
+      ),
+    }))
+  : [];
 
   return (
-    <div>
-      {/* Header */}
-      <section className="max-w-6xl mx-auto px-2 py-8 mb-10">
-        <header className="space-y-4">
-          <h1 className="text-6xl font-bold text-[var(--primary-color)]">{lab.name}</h1>
-        </header>
-      </section>
+    <div className="min-h-screen bg-white relative">
 
-      {/* Mission Section */}
-      {lab.mission && lab.mission.length > 0 && (
-        <section className="max-w-6xl mx-auto px-8 pb-16">
-          <h2 className="text-4xl font-bold text-gray-900 border-b-2 pb-4 mb-4">Misión</h2>
-          <p className="text-lg text-gray-700 text-justify leading-relaxed">{lab.mission}</p>
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          background: `
+            radial-gradient(circle at 50% 50%, rgba(255,255,255,1) 20%, rgba(230,240,255,0.5) 40%, rgba(200,220,255,0.3) 60%, rgba(180,200,255,0.2) 80%),
+            radial-gradient(circle at 20% 20%, rgba(240,240,255,0.2), transparent 70%),
+            radial-gradient(circle at 80% 80%, rgba(240,240,255,0.2), transparent 70%)
+          `,
+        }}
+      ></div>
+
+      <div className="relative z-10">
+        {/* Header */}
+        <section className="max-w-6xl mx-auto px-2 py-8 mb-10">
+          <header className="space-y-4">
+            <h1 className="text-6xl font-bold text-[var(--primary-color)]">{lab.name}</h1>
+          </header>
         </section>
-      )}
-
-      {/* Research Lines Section */}
-      {lab.researchLines && lab.researchLines.length > 0 && (
-        <section className="max-w-6xl mx-auto px-8 pb-16">
-          <h2 className="text-4xl font-bold text-gray-900 border-b-2 pb-4 mb-4">Líneas de Investigación</h2>
-          {Array.isArray(lab.researchLines) ? (
-            <ul className="list-disc list-inside text-lg text-gray-700">
-              {lab.researchLines.map((line, index) => (
-                <li key={index}>{line}</li>
-              ))}
-            </ul>
-          ) : (
-            <p className="text-lg text-gray-700 text-justify leading-relaxed">{lab.researchLines}</p>
-          )}
-        </section>
-      )}
-
-      {/* Services and Equipment Section */}
-      {lab.services && lab.services.length > 0 && (
-        <section className="max-w-6xl mx-auto px-8 pb-16">
-          <h2 className="text-4xl font-bold text-gray-900 border-b-2 pb-4 mb-4">Servicios y Equipo</h2>
-          <Accordion items={accordionItems} />
-        </section>
-      )}
-
-      {/* Photo Gallery Section */}
-      {lab.gallery && lab.gallery.length > 0 && (
-        <section className="max-w-6xl mx-auto px-8 pb-16">
-          <h2 className="text-4xl font-bold text-gray-900 border-b-2 pb-4 mb-4">Galería de Fotos</h2>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-            {lab.gallery.map((image, index) => (
-              <div
-                key={index}
-                className="w-full h-64 bg-gray-200 rounded-lg overflow-hidden shadow-lg"
-              >
-                <img
-                  src={image}
-                  alt={`Imagen ${index + 1}`}
-                  className="w-full h-full object-cover"
-                />
-              </div>
-            ))}
-          </div>
-        </section>
-      )}
-
-      {/* Full-width Aplicaciones Section */}
-      {lab.applications && lab.applications.length > 0 && (
-        <section className="relative w-full space-y-4 mb-16">
-          <div className="absolute inset-0 bg-[var(--primary-color)] opacity-95 z-10"></div>
-          <div
-            className="absolute inset-0 bg-cover bg-repeat-round z-0"
-            style={{ backgroundImage: 'url("/lab/labg.jpg")' }}
-          ></div>
-
-          <div className="relative z-20 px-8">
-            <div style={{ height: '50px' }}></div>
-            <h2 className="text-4xl font-semibold text-[#6fbc7e] pb-4 mb-4">Aplicaciones</h2>
-            <div className="space-y-4 px-10">
-              {lab.applications.map((app, index) => (
-                <ApplicationCard key={index} title={app.title} description={app.description} />
+  
+        {/* Mission Section */}
+        {lab.mission && lab.mission.length > 0 && (
+          <section className="max-w-6xl mx-auto px-8 pb-16">
+            <h2 className="text-4xl font-bold text-gray-900 border-b-2 pb-4 mb-4">Misión</h2>
+            <p className="text-lg text-gray-700 text-justify leading-relaxed">{lab.mission}</p>
+          </section>
+        )}
+  
+        {/* Research Lines Section */}
+        {lab.researchLines && lab.researchLines.length > 0 && (
+          <section className="max-w-6xl mx-auto px-8 pb-16">
+            <h2 className="text-4xl font-bold text-gray-900 border-b-2 pb-4 mb-4">Líneas de Investigación</h2>
+            {Array.isArray(lab.researchLines) ? (
+              <ul className="list-disc list-inside text-lg text-gray-700">
+                {lab.researchLines.map((line, index) => (
+                  <li key={index}>{line}</li>
+                ))}
+              </ul>
+            ) : (
+              <p className="text-lg text-gray-700 text-justify leading-relaxed">{lab.researchLines}</p>
+            )}
+          </section>
+        )}
+  
+        {/* Services and Equipment Section */}
+        {lab.services && lab.services.length > 0 && (
+          <section className="max-w-6xl mx-auto px-8 pb-16">
+            <h2 className="text-4xl font-bold text-gray-900 border-b-2 pb-4 mb-4">Servicios y Equipo</h2>
+            <Accordion items={accordionItems} />
+          </section>
+        )}
+  
+        {/* Photo Gallery Section */}
+        {lab.gallery && lab.gallery.length > 0 && (
+          <section className="max-w-6xl mx-auto px-8 pb-16">
+            <h2 className="text-4xl font-bold text-gray-900 border-b-2 pb-4 mb-4">Galería de Fotos</h2>
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+              {lab.gallery.map((image, index) => (
+                <div
+                  key={index}
+                  className="w-full h-64 bg-gray-200 rounded-lg overflow-hidden shadow-lg"
+                >
+                  <img
+                    src={image}
+                    alt={`Imagen ${index + 1}`}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
               ))}
             </div>
-            <div style={{ height: '50px' }}></div>
-          </div>
-        </section>
-      )}
-
-      {/* Lab Manager Section */}
-      {lab.labManager && lab.labManager.name && (
-        <section className="max-w-6xl mx-auto px-8 pb-16">
-          <h2 className="text-4xl font-bold text-gray-900 border-b-2 pb-4 mb-4">Responsable del Laboratorio</h2>
-          <div className="flex flex-col md:flex-row items-center md:items-start space-y-4 md:space-y-0 md:space-x-4">
-            <img
-              src={lab.labManager.photo}
-              alt="Responsable del laboratorio"
-              className="w-24 h-24 rounded-full shadow-lg object-cover"
-            />
-            <div className="text-justify md:text-justify">
-              <h3 className="text-xl font-semibold text-gray-900 mb-2">{lab.labManager.name}</h3>
-              <div className="text-gray-700">
-                {Array.isArray(lab.labManager.description) ? (
-                  lab.labManager.description.map((desc, index) => (
-                    <p key={index} className="mb-2">
-                      {desc}
-                    </p>
-                  ))
-                ) : (
-                  <p>{lab.labManager.description}</p>
-                )}
+          </section>
+        )}
+  
+        {/* Full-width Aplicaciones Section */}
+        {lab.applications && lab.applications.length > 0 && (
+          <section className="relative w-full space-y-4 mb-16">
+            <div className="absolute inset-0 bg-[var(--primary-color)] opacity-95 z-10"></div>
+            <div
+              className="absolute inset-0 bg-cover bg-repeat-round z-0"
+              style={{ backgroundImage: 'url("/lab/labg.jpg")' }}
+            ></div>
+  
+            <div className="relative z-20 px-8">
+              <div style={{ height: '50px' }}></div>
+              <h2 className="text-4xl font-semibold text-[#6fbc7e] pb-4 mb-4">Aplicaciones</h2>
+              <div className="space-y-4 px-10">
+                {lab.applications.map((app, index) => (
+                  <ApplicationCard key={index} title={app.title} description={app.description} />
+                ))}
+              </div>
+              <div style={{ height: '50px' }}></div>
+            </div>
+          </section>
+        )}
+  
+        {/* Lab Manager Section */}
+        {lab.labManager && lab.labManager.name && (
+          <section className="max-w-6xl mx-auto px-8 pb-16">
+            <h2 className="text-4xl font-bold text-gray-900 border-b-2 pb-4 mb-4">Responsable del Laboratorio</h2>
+            <div className="flex flex-col md:flex-row items-center md:items-start space-y-4 md:space-y-0 md:space-x-4">
+              <img
+                src={lab.labManager.photo}
+                alt="Responsable del laboratorio"
+                className="w-24 h-24 rounded-full shadow-lg object-cover"
+              />
+              <div className="text-justify md:text-justify">
+                <h3 className="text-xl font-semibold text-gray-900 mb-2">{lab.labManager.name}</h3>
+                <div className="text-gray-700">
+                  {Array.isArray(lab.labManager.description) ? (
+                    lab.labManager.description.map((desc, index) => (
+                      <p key={index} className="mb-2">{desc}</p>
+                    ))
+                  ) : (
+                    <p>{lab.labManager.description}</p>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-      )}
+          </section>
+        )}
+      </div>
     </div>
   );
+  
 };
 
 export default LabDetailPage;
